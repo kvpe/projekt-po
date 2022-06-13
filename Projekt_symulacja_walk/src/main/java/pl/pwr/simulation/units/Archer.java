c99b92e69e0a8cc11df266596883918923b8d68a
Delmeus <93483813+Delmeus@users.noreply.github.com>
2022-06-13 20:49:25 +0200

    Delete Projekt_symulacja_walk directory

--- Projekt_symulacja_walk/src/main/java/pl/pwr/simulation/units/Archer.java
+++
@@ -1,9 +0,0 @@
-package pl.pwr.simulation.units;
-
-public class Archer extends Unit {
-    public static int range; //static?
-    public Archer(int size) {
-        super(50, 50, (float)size/4);
-        range = size/5;
-    }
-}
--- Projekt_symulacja_walk/src/main/java/pl/pwr/simulation/units/Soldier.java
+++
@@ -1,7 +0,0 @@
-package pl.pwr.simulation.units;
-
-public class Soldier extends Unit {
-    public Soldier(int size) {
-        super(100,45,(float)size/6);
-    }
-}
